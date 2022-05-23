eaba1f33806db8a5047909b3fe5806b014b07cb7
samschool789@gmail.com <samschool789@gmail.com>
2022-05-22 21:29:00 -0500

    animal results

--- server.js
+++ server.js
@@ -43,9 +43,18 @@ function filterByQuery(query, animalsArray) {
   }
 
 
-
-
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
